06dcb91f4ddfd1846245389464060fa146244775
3dots <3dot@live.ca>
2023-12-23 09:55:16 -0500

    Trivial example success

---
+++ Sandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CLI;
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity e = new Entity("The Wallman", 20, 35);
+            e.Move(5, -10);
+            Console.WriteLine(e.XPosition + " " + e.YPosition);
+            Console.ReadKey();
+        }
+    }
+}
