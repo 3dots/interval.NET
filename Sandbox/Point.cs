336a5e8b9e4722f2278eb5d664e27ea1230db1b1
3dots <3dot@live.ca>
2024-05-18 08:39:53 -0400

    Make it focused

---
+++ Sandbox/Point.cs
@@ -0,0 +1,20 @@
+﻿using interval.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox;
+
+class Point
+{
+    public double X;
+    public IntervalDouble Y;
+
+    public Point(double x, IntervalDouble y)
+    {
+        X = x;
+        Y = y;
+    }
+}
--- Sandbox/Succession.cs
+++ Sandbox/Succession.cs
@@ -10,13 +10,21 @@ namespace Sandbox;
 
 class Succession
 {
-    static readonly int POINTS_NO = 1000;
+    #region Fields
+
+    static readonly int POINTS_NO_INITIAL = 1000;
+    static readonly double POS_THRESHOLD = 0.001;
+    static readonly int POINTS_NO_FOCUS = 1000;
 
     static readonly int M1 = 1000;
     static readonly int N1 = 500;
 
     static readonly int M2 = 1000;
     static readonly int N2 = 300;
+
+    #endregion
+
+    #region Events
 
     public Succession()
     {
@@ -25,90 +33,81 @@ class Succession
 
     public void Run()
     {
-        List<double> points = LinSpace(0, 1, POINTS_NO);
-        List<IntervalDouble> distList = new();
-        foreach (double point in points)
-        {
-            //string? format = "N15";
-            //format = null;
-            IntervalDouble dist = dist1(point);
-            distList.Add(dist);
-            //Console.WriteLine($"{point.ToString(format)} {dist}");
-        }        
+        double[] points = LinSpace(0, 1, POINTS_NO_INITIAL);
+        List<Point> distList = new();
+        Compute(distList, points, dist1);
+
+        int indexFirstRealValue = distList.FindIndex(p => p.Y.Lower >= POS_THRESHOLD);
+        int indexLastRealValue = distList.FindIndex(indexFirstRealValue, p => p.Y.Lower < POS_THRESHOLD);
+
+        points = LinSpace(distList[indexFirstRealValue].X, distList[indexLastRealValue].X, POINTS_NO_FOCUS);
+        Compute(distList, points, dist1);
 
         Runtime.PythonDLL = "C:\\Users\\3dot\\AppData\\Local\\Programs\\Python\\Python312\\python312.dll";
         PythonEngine.Initialize();
         using (Py.GIL())
+        using (PyModule scope = Py.CreateScope())
         {
+            scope.Import("matplotlib.pyplot", "plt");
 
-            using (PyModule scope = Py.CreateScope())
+            int count = distList.Count;
+            double[] pointsArr = new double[count];
+            double[] dist_lower = new double[count];
+            double[] dist_upper = new double[count];
+
+            for (int i = 0; i < distList.Count; i++)
             {
-                scope.Import("matplotlib.pyplot", "plt");
-
-                double[] pointsArr = points.ToArray();
-
-                int count = distList.Count;
-                double[] dist_lower = new double[count];
-                double[] dist_upper = new double[count];
-
-                for (int i = 0; i < distList.Count; i++)
-                {
-                    IntervalDouble dist = distList[i];
-                    dist_lower[i] = dist.Lower;
-                    dist_upper[i] = dist.Upper;
-                }
-
-                PyObject pointsPy = pointsArr.ToPython();
-                PyObject dist_lowerPy = dist_lower.ToPython();
-                PyObject dist_upperPy = dist_upper.ToPython();
-
-                scope.Set(nameof(pointsPy), pointsPy);
-                scope.Set(nameof(dist_lowerPy), dist_lowerPy);
-                scope.Set(nameof(dist_upperPy), dist_upperPy);
-
-                scope.Exec($"plt.figure(figsize=(6, 8))");
-                scope.Exec($"plt.scatter({nameof(pointsPy)}, {nameof(dist_lowerPy)}, s=1)");
-                scope.Exec($"plt.scatter({nameof(pointsPy)}, {nameof(dist_upperPy)}, s=1)");
-                scope.Exec("plt.tight_layout()");
-                scope.Exec("plt.show()");
-
-                Console.WriteLine("Press any key to exit.");
-                Console.ReadKey();
+                Point dist = distList[i];
+                pointsArr[i] = dist.X;
+                dist_lower[i] = dist.Y.Lower;
+                dist_upper[i] = dist.Y.Upper;
             }
 
+            PyObject pointsPy = pointsArr.ToPython();
+            PyObject dist_lowerPy = dist_lower.ToPython();
+            PyObject dist_upperPy = dist_upper.ToPython();
 
-            //dynamic np = Py.Import("numpy");
-            //Console.WriteLine(np.cos(np.pi * 2));
+            scope.Set(nameof(pointsPy), pointsPy);
+            scope.Set(nameof(dist_lowerPy), dist_lowerPy);
+            scope.Set(nameof(dist_upperPy), dist_upperPy);
 
-            //dynamic sin = np.sin;
-            //Console.WriteLine(sin(5));
-
-            //double c = (double)(np.cos(5) + sin(5));
-            //Console.WriteLine(c);
-
-            //dynamic a = np.array(new List<float> { 1, 2, 3 });
-            //Console.WriteLine(a.dtype);
-
-            //dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
-            //Console.WriteLine(b.dtype);
-
-            //Console.WriteLine(a * b);          
+            scope.Exec($"plt.figure(figsize=(6, 8))");
+            scope.Exec($"plt.scatter({nameof(pointsPy)}, {nameof(dist_lowerPy)}, s=1)");
+            scope.Exec($"plt.scatter({nameof(pointsPy)}, {nameof(dist_upperPy)}, s=1)");
+            scope.Exec("plt.tight_layout()");
+            scope.Exec("plt.show()");
         }
     }
 
-    List<double> LinSpace(double start, double end, int numPoints)
+    #endregion
+
+    #region Helpers
+
+    double[] LinSpace(double start, double end, int numPoints)
     {
-        List<double> result = new();
+        double[] result = new double[numPoints];
 
         double step = (end - start) / (numPoints - 1);
 
         for (int i = 0; i < numPoints; ++i)
         {
-            result.Add(start + step * i);
+            result[i] = start + step * i;
         }
 
         return result;
     }
+
+    void Compute(List<Point> list, double[] points, Func<double, IntervalDouble> dist)
+    {
+        foreach (double point in points)
+        {
+            list.Add(new Point(point, dist(point)));
+        }
+    }
+
+    #endregion
+
+    #region Distributions
 
     IntervalDouble dist1(double p) => dist(p, M1, N1);
     double dist1_lower(double p) => dist1(p).Lower;
@@ -146,4 +145,6 @@ class Succession
 
         return factor;
     }
+
+    #endregion
 }
